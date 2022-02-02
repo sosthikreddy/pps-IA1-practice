bee6dfaa266e4c7d949cdfedcc4a3825427383c3
Sosthik R <96185139+sosthikreddy@users.noreply.github.com>
2022-02-02 04:28:48 +0000

    ..

--- p1original.c
+++ p1original.c
@@ -1,2 +1,28 @@
-
+#include<stdio.h>
+int input()
+{
+  printf("enter the number\n");
+  int a;
+  scanf("%d",&a);
+  return a;
+}
+int add(int *a,int *b)
+{
+  int c;
+  c=*a+*b;
+  printf("sum of %d+%d",*a,*b);
+  return c;
+}
+int output(int *g)
+{
+  printf(" is %d",*g);
+}
+int main()
+{
+  int x,y,z;
+  x=input();
+  y=input();
+  z=add(&x,&y);
+  output(&z);
+  return 0;
 }
